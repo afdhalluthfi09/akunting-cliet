11474e5e96c3580ee4662e7c3e13d64537f1ccd8
afdhalluthfi09 <afdhal.luthfi@students.amikom.ac.id>
2020-12-21 07:35:05 +0700

    tambahan file lagi

--- src/views/transaksi/index.vue
+++ src/views/transaksi/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <h1>ini halaman index</h1>
+  <h1>ini halaman index video lanjut malam nanti insya allah di waktu 40.08</h1>
+  <div class="container my-5">
+    <div class="row justify-content-center">
+      <div class="col-8">
+        <router-link
+          :to="{ name: 'transaksi.create' }"
+          class="btn btn-primary btn-sm rounded shadow mb-3"
+        >
+          Add
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
